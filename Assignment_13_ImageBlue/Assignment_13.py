e6ef8e955a8484ff40f1917e86ad820328fd327c
jfong86 <23439258+jfong86@users.noreply.github.com>
2019-12-13 18:46:14 -0500

    Image Blue

--- Assignment_13_ImageBlue/Assignment_13.py
+++ Assignment_13_ImageBlue/Assignment_13.py
@@ -12,13 +12,13 @@ import numpy as np
 image = input ("Enter name of the input file: ")
 image2 = input ("Enter name of the output file: ")
 
-img = plt.imread('csBridge.png')   #Read in image from csBridge.png
-#plt.imshow(img)		           #Load image into pyplot
+img = plt.imread('csBridge.png')    #Read in image from csBridge.png
+#plt.imshow(img)		    #Load image into pyplot
 #plt.show()                         #Show the image (waits until closed to continue)
 
 img2 = img.copy()        #make a copy of our image
+img2[:,:,0] = 0          #Set the red channel to 0
 img2[:,:,1] = 0          #Set the green channel to 0
-img2[:,:,0] = 0        #Set the red channel to 0
 
 #plt.imshow(img2)         #Load our new image into pyplot
 #plt.show()               #Show the image (waits until closed to continue)
